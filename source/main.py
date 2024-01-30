4bf5e0129963cfa362d388ce6d04fbd13f95050f
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-01-30 07:26:50 +0000

    Service handler

---
+++ source/main.py
@@ -0,0 +1,31 @@
+from beatmaps.service import BeatmapsService
+from common.service import Service
+from typing import List
+
+import common.app as app
+import signal
+import sys
+
+class ServiceHandler:
+    
+    def __init__(self) -> None:
+        self.services: List[Service] = self.get_services()
+        signal.signal(signal.SIGTERM, self.signal_handler)
+        signal.signal(signal.SIGINT, self.signal_handler)
+    
+    def signal_handler(sig, frame):
+        app.STOPPED = True
+    
+    def get_services(self):
+        # TODO: disable services based on config
+        return [BeatmapsService()]
+    
+    def run(self):
+        for service in self.services:
+            service.thread.start()
+        for service in self.services:
+            service.thread.join()
+        sys.exit(0)
+
+if __name__ == '__main__':
+    ServiceHandler().run()
