66f8b1c9ba490a42896a386ccfb3ec5281a296d7
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-02-01 11:39:26 +0000

    Logging, update submodule

--- source/main.py
+++ source/main.py
@@ -3,11 +3,13 @@ from common.service import Service
 from typing import List
 
 import common.app as app
+import logging
 import signal
 import sys
 
 class ServiceHandler:
     
+    logger = logging.getLogger("ServiceHandler")
     def __init__(self) -> None:
         self.services: List[Service] = self.get_services()
         signal.signal(signal.SIGTERM, self.signal_handler)
@@ -25,6 +27,7 @@ class ServiceHandler:
             service.thread.start()
         for service in self.services:
             service.thread.join()
+            self.logger.warning(f"Service {service.service_name} exited!")
         sys.exit(0)
 
 if __name__ == '__main__':
