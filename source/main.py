2784b00fa423b237da5ba5018b7936bb3e32eaa2
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-02-02 00:27:03 +0000

    update submodule

--- source/main.py
+++ source/main.py
@@ -1,7 +1,7 @@
-from beatmaps.service import BeatmapsService
 from common.service import Service
 from typing import List
 
+import beatmaps.service as beatmaps_svc
 import common.app as app
 import logging
 import signal
@@ -15,12 +15,12 @@ class ServiceHandler:
         signal.signal(signal.SIGTERM, self.signal_handler)
         signal.signal(signal.SIGINT, self.signal_handler)
     
-    def signal_handler(sig, frame):
+    def signal_handler(self, sig, frame):
         app.STOPPED = True
     
     def get_services(self):
         # TODO: disable services based on config
-        return [BeatmapsService()]
+        return [beatmaps_svc.get_service()]
     
     def run(self):
         for service in self.services:
