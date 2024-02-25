9d5ebda2ac81d04af12f252f9bbb59d0503efb66
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-02-25 11:25:23 +0100

    update submodules

--- source/main_api.py
+++ source/main_api.py
@@ -1,5 +1,8 @@
+from api import app
+# Register API routes
+from api import v1
+
 import uvicorn
-from api.app import app
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
