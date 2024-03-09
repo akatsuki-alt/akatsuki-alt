a90d634c456b764bfaf629ae07299151916c6364
kanaarima <137962405+kanaarima@users.noreply.github.com>
2024-03-09 04:03:28 +0100

    Use custom logging for API Update submodules

--- source/main_api.py
+++ source/main_api.py
@@ -1,3 +1,4 @@
+from api.logger import logger, LogMiddleware
 from api import app
 # Register API routes
 from api import v1
@@ -5,4 +6,5 @@ from api import v1
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=4269)
+    app.add_middleware(LogMiddleware)
+    uvicorn.run(app, host="0.0.0.0", port=4269, log_config=None)
